e5a202051c291ae7e0cf44a782faedad6b616707
Jose Barrera <jbarrera@H5753.ad.hugeinc.com>
2019-02-08 11:47:36 -0500

    Adding AppLiTools Visual test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,16 +1,25 @@
 import { AppPage } from './pageobjects/app.po';
 import { browser, logging } from 'protractor';
 
+// Initialize the eyes SDK and set your private API key.
+var Eyes = require("eyes.selenium").Eyes;
+var eyes = new Eyes();
+eyes.setApiKey("FoLZm17nLHd1IjxD98SCyidR0CT0kSPvSaE101Riqh41gg110");
+
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    eyes.open(browser, "Hello World!", "My first Protractor Test!");
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('Today at Huge.');
+    eyes.checkWindow("Hello!");
+    eyes.checkWindow("Click!");
+
   });
 
   afterEach(async () => {
@@ -19,5 +28,6 @@ describe('workspace-project App', () => {
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
     }));
+    eyes.close();
   });
 });
